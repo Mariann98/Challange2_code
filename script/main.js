c234c45a5642f9b5f05ce43fcf284ce0110f09cc
Mariann <mariann.alber98@gmail.com>
2022-05-30 14:31:40 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,45 @@
+//live time and date
+setInterval(function () {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  minutes = tickingClockStarts(minutes);
+  seconds = tickingClockStarts(seconds);
+
+  document.querySelector("#digital_clock_time").innerHTML =
+    hours + " : " + minutes + " : " + seconds;
+  document.querySelector("#clocks_date").innerHTML = date.toDateString();
+
+  if (hours <= 11) {
+    document.querySelector("#am_pm").innerHTML = "AM";
+  } else {
+    document.querySelector("#am_pm").innerHTML = "PM";
+  }
+});
+
+let tickingClockStarts = value => {
+  if (value < 10) {
+    value = "0" + value;
+  }
+  return value;
+};
+
+//background changes from light to dark mode
+let date = new Date();
+let hours = date.getHours();
+if (hours <= 11) {
+  document.body.style.backgroundColor = "#EFD9AF";
+} else {
+  document.body.style.backgroundColor = "#202020";
+}
+
+//clicking on a button changes time's color
+time_button.addEventListener("click", function () {
+  let currentColor = getComputedStyle(digital_clock_time, null).getPropertyValue("color");
+
+  currentColor === "rgb(192, 166, 241)"
+    ? (digital_clock_time.style.color = "rgb(209, 97, 97)")
+    : (digital_clock_time.style.color = "rgb(192, 166, 241)");
+});
